7102f3e68eedbc373673be97eb84b322e316123c
TatiYas <iasinovska_lawyer@ukr.net>
2024-07-19 22:27:21 +0200

    Update_14

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -1,10 +1,12 @@
 import "./Description.module.css";
 export default function Description() {
   return (
-    <div>
-            <h1 className={s.title}>Sip Happens Café</h1>
-            <div className={s.span}></div>
-            <p className={s.text}>Please leave your feedback about our service by selecting one of the options below.</p>
-        </div>
+    <>
+      <h1>Sip Happens Café</h1>
+      <p>
+        Please leave your feedback about our service by selecting one of the
+        options below.
+      </p>
+    </>
   );
 }

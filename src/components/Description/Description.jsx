43fbf1381887a6f3b6e61839dd7dc20b5448a8fd
TatiYas <iasinovska_lawyer@ukr.net>
2024-07-27 15:58:50 +0200

    Update_27

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -6,6 +6,8 @@
             <div>
                 <h1 className={s.title}>Sip Happens Café</h1>
                 <div className={s.span}></div>
+                <br />
+                <div className={s.span2}></div>
                 <p className={s.text}>Please leave your feedback about our service by selecting one of the options below.</p>
             </div>
         )
